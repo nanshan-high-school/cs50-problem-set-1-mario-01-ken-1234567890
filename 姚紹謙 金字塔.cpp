8840f88ab50f37bc14dffc5117e5b0da1a9752f8
ken-1234567890 <56679717+ken-1234567890@users.noreply.github.com>
2019-12-04 18:56:19 +0800

    Create 姚紹謙 金字塔.cpp

---
+++ 姚紹謙 金字塔.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    int high;
+    do {
+        cout << "請輸入高";
+        cin >> high;
+    }while (high > 8 || high < 1);    
+    
+    
+    for (int i = 0 ; i < high ; i++) {
+    
+        for (int j = 0 ; j < high ; j++){
+        
+            if (i + j < high - 1)
+                cout << " ";
+            else
+                cout << "#";
+        }
+        cout << "\n";
+    }
+    
+  
+    
+    
+    
+    
+
+
+}
